4ba2d44a23cff4902748bcf10d81b7407901f1ee
Ahmed El-Hassany <eahmed@ethz.ch>
2018-04-28 16:55:05 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='Tekton',
+    version='0.1',
+    description='Annotated Computer Network Graph',
+    author='Ahmed El-Hassany',
+    author_email='a.hassany@gmail.com',
+    url='https://synet.etzh.ch',
+    license = "Apache",
+    packages=['tekton'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache License",
+    ],
+)
